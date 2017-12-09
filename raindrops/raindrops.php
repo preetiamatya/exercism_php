f8808cbf1f8b9a3f1f8287fd755e1222b139e03b
Preeti Amatya <preetiamatya@gmail.com>
2017-12-09 10:08:46 +0100

    optimization raindrops

--- raindrops/raindrops.php
+++ raindrops/raindrops.php
@@ -1,17 +1,18 @@
 <?php
-function raindrops($num) {
-  $string  = '';
-  if ((int)($num % 3) == 0) {
-    $string.="Pling";
-  }
-  if ((int)($num % 5)== 0){
-    $string.="Plang";
-  }
-  if ((int)($num % 7)== 0) {
-    $string.= "Plong";
-  }
-  if ((int)($num % 3) != 0 && (int)($num % 5) != 0 &&(int)($num % 7) != 0) {
-    $string.=  (string)$num;
-  }
-  return $string;
+function raindrops($num)
+{
+    $string = '';
+    if ((int) ($num % 3) == 0) {
+        $string .= "Pling";
+    }
+    if ((int) ($num % 5) == 0) {
+        $string .= "Plang";
+    }
+    if ((int) ($num % 7) == 0) {
+        $string .= "Plong";
+    }
+    if ((int) ($num % 3) != 0 && (int) ($num % 5) != 0 && (int) ($num % 7) != 0) {
+        $string .= (string) $num;
+    }
+    return $string;
 }
